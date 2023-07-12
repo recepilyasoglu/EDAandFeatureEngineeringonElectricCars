44b8a48549e5b8815f139aca33382612b6282931
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-07-12 11:18:28 +0300

    EDA & Feature Engineering on Electric Cars Dataset

---
+++ eda-feature_engineering.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import matplotlib
+matplotlib.use("Qt5Agg")
+
+pd.set_option("display.width", 500)
+pd.set_option("display.max_columns", None)
+
+df = pd.read_csv("Electric_Vehicle_Population_Data.csv")
+df.head()
+
+# EDA
+def get_stats(dataframe):
+    return print("############### First 5 Line ############### \n", dataframe.head(), "\n", \
+                 "############### Number of Values Owned ############### \n", dataframe.value_counts(), "\n", \
+                 "############### Total Number of Observations ############### \n", dataframe.shape, "\n", \
+                 "############### Variables Types ############### \n", dataframe.dtypes, "\n", \
+                 "############### Total Number of Null Values ############### \n", dataframe.isnull().sum(), "\n", \
+                 "############### Descriptive Statistics ############### \n", dataframe.describe().T
+                 )
+
+get_stats(df)
+
+
+def grab_col_names(dataframe, cat_th=2, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+df[cat_cols]
+df[num_cols].head()
+df[cat_but_car].head()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
